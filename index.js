8f1ded4f26623e3a9d3ede01522a4b2d38972d32
Noah Kalkstein <nkalkstein@gmail.com>
2017-11-16 22:16:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ function lowerCaseDrivers(drivers) {
 function nameToAttributes(drivers){
   firstName = []
   lastName = []
-  
+
   const newDrivers = drivers.map(function (name) {
     return Object.assign({}, name, {firstName: 'name' });
   });
 
   newDrivers;
+}
+
+function attributesToPhrase(drivers) {
+  const attributes = drivers.map(function (account) {
+    return `${account.name} is from ${account.hometown}`;
+  });
 }
