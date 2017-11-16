ba5ecb2682c0376d2805aa9792277c3ea219904f
Noah Kalkstein <nkalkstein@gmail.com>
2017-11-16 22:06:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function lowerCaseDrivers(drivers) {
   const newList = drivers.map(function(name) {return name.toLowerCase();})
   return newList
 }
+
+function nameToAttributes(drivers){
+  const newDrivers = drivers.map(function (name) {
+    return Object.assign({}, name, { equipment: 'Laptop' });
+  });
+   
+  newDrivers;
+}
